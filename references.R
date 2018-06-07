79427247044a197fcb9f62be7f5c95ab58d1b286
Alastair Harry <alastair.harry@gmail.com>
2018-06-06 23:58:56 -0300

    references

--- references.R
+++ references.R
@@ -1,23 +1,35 @@
 library(rvest)
 library(tidyverse)
 
+## Function for scraping references from SharkReferences.com
 scrape_refs <- function(letter = "A"){
 
-data <- read_html(paste0("http://shark-references.com/literature/listAll/", letter))
+# Read in html
+my_link <- read_html(paste0("http://shark-references.com/literature/listAll/", letter))
 
-Test <- data %>% 
+# 
+data <- my_link %>% 
   html_node(".list") %>%
   html_text() %>%
   str_split(pattern = "\n\t\t\t\t\t\t\n\t\t") %>%
   tibble() %>%  unnest() %>% .[1] %>%
   rename(resource = ".")  %>% 
   mutate(resource = str_trim(resource)) %>% 
-  mutate(year = str_extract(resource, "[:digit:]+")) %>%
+  mutate(year = str_extract(resource, "[:digit:]{4}")) %>%
   separate(resource, into = c("resource", "DOI"), sep = "DOI: ") %>% 
   mutate(resource = str_replace_all(resource, "\n\t\t\t", " ")) %>%
   mutate(resource = str_replace_all(resource, "\t\t\t\t", "")) %>% 
+  mutate(source = "Shark-references") %>% 
+  arrange(year) %>% 
   filter(!is.na(resource))
 
-return(Test)
+return(data)
 }
 
+reference_list <- map_df(list("A", "B"), scrape_refs)
+
+
+write_rds(reference_list)
+
+
+# Refresh web-scraping
