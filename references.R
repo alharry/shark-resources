244b93b619a946398be84b7a27f08067994b1e1f
Alastair Harry <alastair.harry@gmail.com>
2018-06-10 19:36:56 -0300

    shark-resources

--- references.R
+++ references.R
@@ -16,20 +16,49 @@ data <- my_link %>%
   rename(resource = ".")  %>% 
   mutate(resource = str_trim(resource)) %>% 
   mutate(year = str_extract(resource, "[:digit:]{4}")) %>%
+  mutate(year = as.numeric(year)) %>% 
   separate(resource, into = c("resource", "DOI"), sep = "DOI: ") %>% 
   mutate(resource = str_replace_all(resource, "\n\t\t\t", " ")) %>%
   mutate(resource = str_replace_all(resource, "\t\t\t\t", "")) %>% 
   mutate(source = "Shark-references") %>% 
   arrange(year) %>% 
-  filter(!is.na(resource))
+  filter(!resource == "") %>% 
+  mutate(resource_id = NA) %>% 
+  select(resource, resource_id, year, everything())
 
 return(data)
 }
 
-reference_list <- map_df(list("A", "B"), scrape_refs)
+## Step 1. Load reference list and log file
+reference_list <- read_rds("shark-resources.rds") 
+log_file <- read_rds("log.rds")
+  
+## Step 2. Get new references
+  # First get all references from Shark References 
+new_references <- map_df(LETTERS, scrape_refs) %>%
+  # Use anti_join to separate out the new ones
+  anti_join(reference_list, by = "resource") %>% 
+  mutate(resource_id = as.numeric(resource_id))
+  
+  # Now load up the other references
+other_references <- readxl::read_xlsx("other-references.xlsx",
+                                      col_types = c("numeric", "text", "text")) %>%
+  # Use anti_join to separate out the new ones
+  anti_join(reference_list, by = "resource")
 
+## Step 3. Join new and old references
+reference_list <- full_join(new_references, other_references) %>% 
+  arrange(year) %>% 
+  mutate(resource_id = row_number() + max(reference_list$resource_id)) %>% 
+  full_join(reference_list) %>%
+  arrange(resource_id)
 
-write_rds(reference_list)
+## Create a new log file entry
+log_file <- add_row(log_file, 
+                    `date-accessed` = format(Sys.time(), "%b %d %Y %X"),
+                    `shark-references` = table(reference_list$source)[2],
+                    `other-references` = table(reference_list$source)[1])
 
+write_rds(reference_list, "shark-resources.rds")
+write_rds(log_file, "log.rds")
 
-# Refresh web-scraping
